acee323096ec05ac5bf7b0a4f18f8950c0a8b3bc
Saahil Rachh <60090957+SAL778@users.noreply.github.com>
2022-10-06 11:32:05 -0600

    feat: fact function

--- fact.cpp
+++ fact.cpp
@@ -3,10 +3,18 @@ using namespace std;
 
 //function goes here
 
+int fact(int n)
+{
+    if(n>1){return n * fact(n-1);}
+    else {return 1;}
+}
+
+
 int main()
 {
   cout<<"Enter the number:"<<endl;
   int n; cin>>n;
   
   //call function here
+  cout<<"The factorial is "<<fact(n)<<endl;
 }
